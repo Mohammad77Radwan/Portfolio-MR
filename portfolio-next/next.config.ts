37a6e9fa0732df0b795ee3fa11ae0c1bd2f49336
Mohammad77Radwan <mohammadradwan804@gmail.com>
2026-04-05 12:32:18 +0000

    fix: static export and disable image optimization for Netlify

--- portfolio-next/next.config.ts
+++ portfolio-next/next.config.ts
@@ -2,6 +2,13 @@
 import type { NextConfig } from "next";
 
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
+};
+
+export default nextConfig;
 
 
