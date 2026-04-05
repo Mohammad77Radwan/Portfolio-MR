94e3b01ed53812f65b5354cb0231a270c8e5f704
Mohammad77Radwan <mohammadradwan804@gmail.com>
2026-04-05 09:15:15 +0000

    fix: resolve workspace root and path alias issues for Netlify, add prisma helper

--- portfolio-next/next.config.ts
+++ portfolio-next/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+
+const path = require('path');
+
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbopack: {
+      root: path.resolve(__dirname),
+    },
+  },
 };
 
 export default nextConfig;
